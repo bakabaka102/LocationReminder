450009f15b5ae3570cb7421929bcb2dd36858abc
H Ngg <hnn@gmail.com>
2024-05-18 00:46:29 +0700

    - Update permission + Logic P2 - Refactor

--- app/src/main/java/com/udacity/project4/authentication/AuthenticationActivity.kt
+++ app/src/main/java/com/udacity/project4/authentication/AuthenticationActivity.kt
@@ -42,18 +42,17 @@ class AuthenticationActivity : BaseActivity<ActivityAuthenticationBinding>() {
     }
 
     override fun initViews() {
+
+    }
+
+    override fun initActions() {
         // TODO: Implement the create account and sign in using FirebaseUI,
         //  use sign in using email and sign in using Google
         // TODO: If the user was authenticated, send him to RemindersActivity
         // TODO: a bonus is to customize the sign in flow to look nice using :
         //https://github.com/firebase/FirebaseUI-Android/blob/master/auth/README.md#custom-layout
-    }
-
-    override fun initActions() {
         mBinding.btnLogin.setOnClickListener {
-            //startRemindersActivity()
             launchSignInFlow()
-
         }
     }
 
